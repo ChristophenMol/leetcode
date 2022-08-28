f7bcb2083842212042712de23dfeaf4793a97e81
Christopher Molnar <86855616+ChristophenMol@users.noreply.github.com>
2022-08-28 03:18:40 -0700

    Create RomanInterger.java
    Unary solution.

---
+++ RomanInterger.java
@@ -0,0 +1,19 @@
+
+class Solution {
+
+public String getRomanNumber(int number) {
+    return  ("I".repeat(number)"))
+            .replace("IIIII", "V")
+            .replace("IIII", "IV")
+            .replace("VV", "X")
+            .replace("VIV", "IX")
+            .replace("XXXXX", "L")
+            .replace("XXXX", "XL")
+            .replace("LL", "C")
+            .replace("LXL", "XC")
+            .replace("CCCCC", "D")
+            .replace("CCCC", "CD")
+            .replace("DD", "M")
+            .replace("DCD", "CM");
+}
+}
